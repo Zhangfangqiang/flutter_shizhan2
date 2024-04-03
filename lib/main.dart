2dd75bc172cebf9536bd717aabf63fdd7021e3a1
zf <zf18600004319@aliyun.com>
2024-04-03 17:49:22 +0800

    完成

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,46 +22,82 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-
         /*外部必须套一个Navigator*/
-        home: Home()
-    );
+        home: Container(child: FocusTestRoute()));
   }
 }
 
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return       TextButton(
-      child: Text("open new route"),
-      onPressed: () {
-        // 导航到新路由
-        Navigator.push(
-          context,
+class FocusTestRoute extends StatefulWidget {
+  const FocusTestRoute({Key? key}) : super(key: key);
 
-          /*创建一个路由页面*/
-          MaterialPageRoute(builder: (context) {
-            return NewRoute();
-          }),
-        );
-      },
-    );
-  }
+  @override
+  _FocusTestRouteState createState() => _FocusTestRouteState();
 }
 
-/**
- * 路由页面具体填充的组件
- */
-class NewRoute extends StatelessWidget {
+class _FocusTestRouteState extends State<FocusTestRoute> {
+  FocusNode focusNode1 = FocusNode();
+  FocusNode focusNode2 = FocusNode();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("New route"),
+    return Material(
+        child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: <Widget>[
+          TextField(
+            autofocus: true,
+            focusNode: focusNode1, //关联focusNode1
+            toolbarOptions: const ToolbarOptions(
+              selectAll: true,
+              copy: true,
+              paste: true,
+            ),
+            decoration: const InputDecoration(labelText: "input1"),
+          ),
+          TextField(
+            focusNode: focusNode2, //关联focusNode2
+            decoration: const InputDecoration(labelText: "input2"),
+          ),
+          Builder(
+            builder: (ctx) {
+              return Column(
+                children: <Widget>[
+                  ElevatedButton(
+                    child: const Text("移动焦点"),
+                    onPressed: () {
+                      //将焦点从第一个TextField移到第二个TextField
+                      FocusScope.of(context).requestFocus(focusNode2);
+                    },
+                  ),
+                  ElevatedButton(
+                    child: const Text("隐藏键盘"),
+                    onPressed: () {
+                      // 当所有编辑框都失去焦点时键盘就会收起
+                      focusNode1.unfocus();
+                      focusNode2.unfocus();
+                    },
+                  ),
+                ],
+              );
+            },
+          ),
+
+          const Text("自定义下划线颜色"),
+          const TextField(
+            decoration: InputDecoration(
+              labelText: "请输入用户名",
+              prefixIcon: Icon(Icons.person),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.green),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.red),
+              ),
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        child: Text("This is new route"),
-      ),
-    );
+    ));
   }
 }
