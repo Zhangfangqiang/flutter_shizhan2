e660b8957cf40e20937b7338caf62e70bb875b30
zf <zf18600004319@aliyun.com>
2024-04-03 11:24:31 +0800

    添加简单路由

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,89 +19,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      /*应用名称*/
-      title: 'Flutter Demo',
-      /*主题配置*/
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      /*应用首页路由 */
-      home: ParentWidget(),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+
+        /*外部必须套一个Navigator*/
+        home: Home()
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return       TextButton(
+      child: Text("open new route"),
+      onPressed: () {
+        // 导航到新路由
+        Navigator.push(
+          context,
+
+          /*创建一个路由页面*/
+          MaterialPageRoute(builder: (context) {
+            return NewRoute();
+          }),
+        );
+      },
     );
   }
 }
 
 /**
- * 拥有完整状态的组件
+ * 路由页面具体填充的组件
  */
-class ParentWidget extends StatefulWidget {
-  @override
-  _ParentWidgetState createState() => _ParentWidgetState();
-}
-
-/**
- * ParentWidget 的实现
- */
-class _ParentWidgetState extends State<ParentWidget> {
-  bool _active = false;       //添加一个状态
-
-  /**
-   * 创建一个修改这个状态的方法
-   */
-  void _handleTapboxChanged(bool newValue) {
-    setState(() {
-      _active = newValue;
-    });
-  }
-
+class NewRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: TapboxB(
-        active: _active,
-        onChanged: _handleTapboxChanged,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New route"),
       ),
-    );
-  }
-}
-
-/**
- * 没有完整状态的组件
- */
-class TapboxB extends StatelessWidget {
-  /**
-   * 构造方法
-   * 给 active 一个默认值
-   * onChanged 是一个必须填写的方法
-   */
-  TapboxB({Key? key, this.active = false, required this.onChanged}) : super(key: key);
-
-  final bool active;
-  final ValueChanged<bool> onChanged;
-
-  /**
-   * 给onChanged 方法包一层
-   */
-  void _handleTap() {
-    onChanged(!active);
-  }
-
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _handleTap,
-      child: Container(
-        child: Center(
-          child: Text(
-            active ? 'Active' : 'Inactive',
-            style: TextStyle(fontSize: 32.0, color: Colors.white),
-          ),
-        ),
-        width: 200.0,
-        height: 200.0,
-        decoration: BoxDecoration(
-          color: active ? Colors.lightGreen[700] : Colors.grey[600],
-        ),
+      body: Center(
+        child: Text("This is new route"),
       ),
     );
   }
